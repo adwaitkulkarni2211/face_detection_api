e738ce294b4ebe1516aff11424d726e64032ec49
Adwait Kulkarni <adwaitkulkarni2211@gmail.com>
2021-01-31 16:57:25 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'trees123@P',
+        database: 'smart_brain'
+    }
+});
+
+db.select('*').from('users').then(data => {
+    
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
+})
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user count
+*/
